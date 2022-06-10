27537beca4dde6479dde035ec84f2ae8d917d9da
angemas <angelique.massolo@outlook.com>
2022-06-10 19:38:21 +0200

    axios & api url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,26 +1,42 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
 
-    let [keyword,setKeyword]=useState("");
+  function getDefinition(response) {
+    console.log(response.data[0]);
+  }
 
-    function handleSubmit(event){
-        event.preventDefault();
-    alert(`Search for ${keyword}`)
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiURL).then(getDefinition);
+  }
 
-    function Handlekeyword(event){
-        event.preventDefault();
-        setKeyword(event.target.value)
-    }
+  function Handlekeyword(event) {
+    event.preventDefault();
+    setKeyword(event.target.value);
+  }
 
   return (
     <div className="Dictionary mt-5">
-        <h1>Dictionary</h1>
-      <form className="Search-form mt-5 d-flex justify-content-center" onSubmit={handleSubmit}>
-        <input type="text" autoFocus="on" placeholder="search" className="Search-bar" onChange={Handlekeyword} />
-      </form>
+      <h1>Dictionary</h1>
+      <div>
+        <form
+          className="Search-form mt-5 d-flex justify-content-center"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            autoFocus="on"
+            placeholder="Search"
+            className="Search-bar"
+            onChange={Handlekeyword}
+          />
+        </form>
+      </div>
     </div>
   );
 }
