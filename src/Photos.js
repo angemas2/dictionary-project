9c1d6a5f0138cff283e1ab82636b536efb71022b
angemas <angelique.massolo@outlook.com>
2022-06-12 19:27:41 +0200

    alt

--- src/Photos.js
+++ src/Photos.js
@@ -18,7 +18,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt={index}
+                    alt=""
                   />{" "}
                 </a>
               </div>
